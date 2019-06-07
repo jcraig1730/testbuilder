e30958edfefa4f89ba02dcc8f6a321726890ac92
Jared Craig <jcraig1730@gmail.com>
2019-06-06 22:59:06 -0500

    Add functionality for detecting China UnionPay

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -14,31 +14,54 @@ var detectNetwork = function(cardNumber) {
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
   let cardPrefix2Digit = Number(cardNumber.slice(0, 2));
+  let cardPrefix3Digit = Number(cardNumber.slice(0, 3));
   let cardPrefix4Digit = Number(cardNumber.slice(0, 4));
-  let cardLength = cardNumber.length
-  if (cardLength === 14 && (cardPrefix2Digit === 38 || cardPrefix2Digit === 39)){
+  let cardPrefix6Digit = Number(cardNumber.slice(0, 6));
+  let cardLength = cardNumber.length;
+  if (
+    cardLength === 14 &&
+    (cardPrefix2Digit === 38 || cardPrefix2Digit === 39)
+  ) {
     return "Diner's Club";
   }
-  if (cardLength === 15 && (cardPrefix2Digit === 34 || cardPrefix2Digit === 37)){
-    return "American Express";
+  if (
+    cardLength === 15 &&
+    (cardPrefix2Digit === 34 || cardPrefix2Digit === 37)
+  ) {
+    return 'American Express';
   }
-  if ((cardLength === 13 || cardLength === 16 || cardLength === 19) && cardNumber[0] === "4"){
-    return "Visa";
+  if (
+    (cardLength === 13 || cardLength === 16 || cardLength === 19) &&
+    cardNumber[0] === '4'
+  ) {
+    return 'Visa';
   }
-  if (cardLength === 16 && (cardPrefix2Digit >= 51 && cardPrefix2Digit <= 55)){
-    return "MasterCard";
+  if (cardLength === 16 && (cardPrefix2Digit >= 51 && cardPrefix2Digit <= 55)) {
+    return 'MasterCard';
   }
-  if ((cardLength === 16 || cardLength === 19) && (cardPrefix4Digit === 6011 || (Number(cardNumber.slice(0, 3)) >= 644 && Number(cardNumber.slice(0, 3)) <= 649 ) || cardPrefix2Digit === 65)){
-    return "Discover";
+  if (
+    (cardLength === 16 || cardLength === 19) &&
+    (cardPrefix4Digit === 6011 ||
+      (cardPrefix3Digit >= 644 && cardPrefix3Digit <= 649) ||
+      cardPrefix2Digit === 65)
+  ) {
+    return 'Discover';
   }
-  let maestroPrefixes = [5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763];
-  if (maestroPrefixes.includes(cardPrefix4Digit) && (cardLength >= 12 && cardLength <= 19)){
-    return "Maestro"
+  let maestroPrefixes = [5018, 5020, 5038, 6304];
+  if (
+    maestroPrefixes.includes(cardPrefix4Digit) &&
+    (cardLength >= 12 && cardLength <= 19)
+  ) {
+    return 'Maestro';
+  }
+  if (
+    cardLength >= 16 &&
+    cardLength <= 19 &&
+    ((cardPrefix6Digit >= 622126 && cardPrefix6Digit <= 622925) ||
+      (cardPrefix4Digit >= 6282 && cardPrefix4Digit <= 6288) ||
+      (cardPrefix3Digit >= 624 && cardPrefix3Digit <= 626) ||
+      (cardPrefix2Digit >= 16 && cardPrefix2Digit <= 19))
+  ) {
+    return 'China UnionPay';
   }
 };
-
-
-// 5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763
-// length 16-19
-
-
